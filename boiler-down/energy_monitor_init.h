f52bdb7c05f7dc6b5a120ed17bdbbc69f65154a0
NikS8 <33399815+NikS8@users.noreply.github.com>
2020-01-04 15:54:55 +0200

    Create energy_monitor_init.h

---
+++ boiler-down/energy_monitor_init.h
@@ -0,0 +1,11 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\
+            init EnergyMonitor
+\*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+//#include "energy_monitor_init.h"
+
+#include <EmonLib.h>
+EnergyMonitor emon1;
+EnergyMonitor emon2;
+EnergyMonitor emon3;
+  
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
