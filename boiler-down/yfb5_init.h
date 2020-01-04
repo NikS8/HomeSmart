74d6c746641b86af21d9ea532be1eb04cc674558
NikS8 <33399815+NikS8@users.noreply.github.com>
2020-01-04 15:47:49 +0200

    Create yfb5_init.h

---
+++ boiler-down/yfb5_init.h
@@ -0,0 +1,14 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\
+            init YF-B5
+\*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+//#include "yfb5_init.h"
+
+#define PIN_YFB5 2
+#define PIN_INTERRUPT_YFB5 0
+#define YFB5_CALIBRATION_FACTOR 5
+//byte yfb5Interrupt = 0; // 0 = digital pin 2
+volatile long yfb5PulseCount = 0;
+unsigned long yfb5LastTime;
+unsigned long currentTime;
+  
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
