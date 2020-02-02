7a9d58394fe99bb0ffd148969e3235e8559c01de
NikS <nik.8@i.ua>
2020-02-02 19:10:39 +0200

    try add match to lan serice

--- collector-floor/collector-floor.ino
+++ collector-floor/collector-floor.ino
@@ -26,6 +26,7 @@
 #include <RBD_Timer.h>            //  DS18B20  pin OneWire D9
 #include <EnableInterrupt.h>      //  flow YF-B5 pins A0,A1,A2,A3,A4,A5,D2,D3
 #include <Servo.h>                //  Servo996R pin 5
+#include <Regexp.h>
 
 //  Блок settings  ------------------------------------------------------------
 #include "collector_floor_init.h"
@@ -44,7 +45,7 @@ void setup() {
   ds18b20Setup();
   yfb5InterruptSetup();
   servo996rSetup();
-
+  
 }
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*\
--- collector-floor/real_time_service.ino
+++ collector-floor/real_time_service.ino
@@ -17,12 +17,35 @@ String readString;
 
       if (c == '\n') {
         // log it in serial pls:
-        Serial.println("readString");
+        Serial.println("readString20");
         Serial.println(readString);
 
+        parseRequest(readString);
         responseData(reqClient);
       }
     }
+  }
+}
+
+void parseRequest(String &readString) {
+
+  // GET / HTTP1.1
+  Serial.print("readString33: ");
+  Serial.println(readString);
+
+  MatchState ms;
+  //ms.Target("POST / HTTP1");
+  ms.Target(readString);
+
+  char buff [100];
+  char result = ms.Match("POST (%a) ", 0);
+  if (result == REGEXP_MATCHED) {
+    Serial.println("matched");
+    Serial.println(ms.GetCapture(buff, 0));
+  } else if (result == REGEXP_NOMATCH) {
+    Serial.println("no match");
+  } else {
+    Serial.println("FAIL");
   }
 }
 
